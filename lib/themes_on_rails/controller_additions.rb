39a84fbae8aa76ad521d03946db70269363ef0ce
James Jeffers <jdjeffers@gmail.com>
2016-09-16 09:57:12 -0400

    skips theme application if theme name is blank

--- lib/themes_on_rails/controller_additions.rb
+++ lib/themes_on_rails/controller_additions.rb
@@ -4,9 +4,11 @@ module ThemesOnRails
 
     module ClassMethods
       def theme(theme, options={})
-        @_theme         = theme
-        @_theme_options = options
-        ThemesOnRails::ActionController.apply_theme(self, theme, options)
+        unless theme.blank?
+          @_theme         = theme
+          @_theme_options = options
+          ThemesOnRails::ActionController.apply_theme(self, theme, options)
+        end
       end
     end
   end
